9378d9ef52eac34451c147093fe15d8448b7decb
Jared Hughes <jahughes241@gmail.com>
2022-07-26 23:16:38 -0700

    Update language data

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,29 @@
 import {parser} from "./syntax.grammar"
-import {LRLanguage, LanguageSupport, indentNodeProp, foldNodeProp, foldInside, delimitedIndent} from "@codemirror/language"
+import {LRLanguage, LanguageSupport, bracketMatching} from "@codemirror/language"
 import {styleTags, tags as t} from "@lezer/highlight"
 
 export const golfScriptLanguage = LRLanguage.define({
   parser: parser.configure({
     props: [
-      indentNodeProp.add({
-        Application: delimitedIndent({closing: ")", align: false})
-      }),
-      foldNodeProp.add({
-        Application: foldInside
-      }),
       styleTags({
         Identifier: t.variableName,
-        Boolean: t.bool,
+        Number: t.number,
         String: t.string,
         LineComment: t.lineComment,
-        "( )": t.paren
+        "{ }": t.brace,
+        ":": t.updateOperator
       })
     ]
   }),
   languageData: {
-    commentTokens: {line: ";"}
+    commentTokens: {line: "#"}
   }
 })
 
 export function golfScript() {
-  return new LanguageSupport(golfScriptLanguage)
+  return new LanguageSupport(
+    golfScriptLanguage,    
+    // limit bracket matching to just the delimeters specified in syntax.grammar
+    bracketMatching({brackets: "{}"})
+  )
 }
